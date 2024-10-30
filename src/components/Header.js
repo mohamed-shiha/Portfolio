a83edcbf1772ff14df4fcdd0263e1d23f409b82f
Mohamed Shiha <mohamedshiha89@gmail.com>
2024-10-30 02:25:26 +0000

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,12 @@
- 
+const Header = () => {
+  return (
+    <header className="bg-gradient-to-r from-blue-600 to-indigo-700 text-white py-20">
+      <div className="container mx-auto px-4">
+        <h1 className="text-5xl font-bold mb-4">Mohamed Shiha</h1>
+        <p className="text-2xl">Software Engineer | Game Developer</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header; 
