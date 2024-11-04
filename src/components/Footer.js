fdeeae3d789d002827575484e71fdac23b85410a
Mohamed Shiha <mohamedshiha89@gmail.com>
2024-11-04 23:07:01 +0000

    added visiters counter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,17 @@ function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-4 text-center">
-        <p className="flex items-center justify-center">
-          <FaCopyright className="mr-2" /> 
-          {new Date().getFullYear()} Mohamed Shiha. All rights reserved.
-        </p>
+        <div className="flex flex-col items-center space-y-4">
+          <p className="flex items-center">
+            <FaCopyright className="mr-2" /> 
+            {new Date().getFullYear()} Mohamed Shiha. All rights reserved.
+          </p>
+          <img 
+            src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fmohamed-shiha.github.io%2FPortfolio&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=visits&edge_flat=false"
+            alt="Visitor Count"
+            className="opacity-75 hover:opacity-100 transition-opacity"
+          />
+        </div>
       </div>
     </footer>
   );
